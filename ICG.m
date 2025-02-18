d9b4f131475cd623b0d87a3e85ead5e6e23153d0
magnesium2400 <98678665+magnesium2400@users.noreply.github.com>
2025-02-18 14:12:53 +1100

    Clean ICG.m
    functionality should be all the same

--- ICG.m
+++ ICG.m
@@ -10,17 +10,12 @@ function [activityICG,outPairID] =  ICG(allData)
 %activityICG - ICG activity for level l
 %outPairID - the ids of original neurons grouped at each level
 
-%Brandon Munn, 19/10/21 
+%Brandon Munn, 19/10/21
 
 
-%%
+%% Prelims (ICG level 1 is just the original data)
 %Calculate how many ICG iterations are possible
-ICGsteps = nextpow2(size(allData,1));
-
-%Catch case if more neurons than a power of 2
-if size(allData,1) == 2^ICGsteps
-    ICGsteps = ICGsteps + 1;
-end
+ICGsteps = nextpow2(size(allData,1)+0.5); % Catch case if more neurons than a power of 2
 
 %The first level is the cellular activity
 activityICG = cell(1,ICGsteps);
@@ -28,115 +23,86 @@ activityICG{1} = allData;
 
 %Cell ids correspond to order inputted
 outPairID = cell(1,ICGsteps);
-outPairID{1}(1:size(allData,1),1) = 1:size(allData,1);
+outPairID{1} = (1:size(allData,1))';
 
 clearvars allData
+
+
 %% Start the ICG process
-
-
 for ICGlevel = 2:ICGsteps
-    fprintf('=== ICG level is %2i out of %2i ===\n', ICGlevel, ICGsteps); 
-    
+    fprintf('=== ICG level is %2i out of %2i ===\n', ICGlevel, ICGsteps);
+
+    %% Setup for current iteration
     %Grab data
     ICGAct = activityICG{ICGlevel-1};
     nData = size(ICGAct,1);
-    
-    
+
+    %How many pairs can be made
+    numPairsTotal = floor(nData/2);
+
+    % I also know the size of my output data = half data by time
+    outdat = nan(numPairsTotal,size(ICGAct,2));
+
+    %this is 2 times number of pairings before
+    outPairID{ICGlevel} = nan(numPairsTotal,2^(ICGlevel-1));
+
+
+    %% Get (sorted) correlations between neurons
     %Calculate correlation matrix
     tic
     rho = corr(ICGAct');
     fprintf('Correlation computation : %f seconds\n', toc);
-    
-    C = triu(rho,1);
-    clearvars rho
-    
-      
-    %Grab just the upper triangle
-    %Find the indices of each element
-    upTriIndx = find(triu(true(size(C)),1)>0);
-    [allRIndx, allCIndx] = ind2sub(size(C),upTriIndx);
-    
-    %now it is a vector of correlation values
-    C = C(upTriIndx);
-    clearvars upTriIndx
-    
-    %Sort the correlation matrix
-    [~,sCI] = sort(C,'descend');
-    
-    
-    %resort row/col indices and turn into pairings 
-    allRowIndx = allRIndx(sCI);
-    allColIndx = allCIndx(sCI);
-    clearvars allRIndx allCIndx C sCI
-    
-   
-    %How many pairs can be made
-    numPairsTotal = floor(nData/2);
-    
-    
-    % I also know the size of my output data = half data by time
-    outdat = nan(numPairsTotal,size(ICGAct,2));
-    
-    %to save pairings
-    outPair = nan(numPairsTotal,2);
-    
-    %this is 2 times number of pairings before
-    outPairID{ICGlevel} = nan(numPairsTotal,2^(ICGlevel-1));
 
-    %% Index counter 
-    k = 1;
+    %Sort edges in this correlation matrix - keep track of the edge order
+    upTriMask = triu(true(nData),1);
+    [~,sCI] = sort(rho(upTriMask),'descend');
+    [allCIndx, allRIndx] = meshgrid(1:nData); 
+    allRIndx    = allRIndx(upTriMask);  allCIndx    = allCIndx(upTriMask); 
+    allRowIndx  = allRIndx(sCI);        allColIndx  = allCIndx(sCI);
+    
+    clearvars allRIndx allCIndx sCI upTriMask rho
+
+
+    %% Match the neurons (greedily)
     gdIndex = true(numel(allRowIndx),1);
-  
+
     tic
     for numPairCnt = 1:numPairsTotal
-         
+
         %Text counter
         if ~mod(numPairCnt,250)
-            fprintf('%6i pairs out of %6i completed (%3i %%) in %f seconds\n', ...
-                numPairCnt, numPairsTotal, round(100*numPairCnt/numPairsTotal), toc);
+            fprintf('%6i pairs out of %6i completed (%2i%%) in %f seconds\n', ...
+                numPairCnt, numPairsTotal, fix(100*numPairCnt/numPairsTotal), toc);
 
         end
-        
-        %Grabbing data
-        %top row index
-        rowNew = allRowIndx(k);
-        %grab corresponding data
-        datRow = ICGAct(rowNew,:);
-        
-        %top col index
-        colNew = allColIndx(k);
-        %grab corresponding data
-        datCol = ICGAct(colNew,:);
-        
-        
-        %ICG process
-        %Sum the data
-        SpikeAct = datRow+datCol;
-        
-        % Save data
-        outdat(numPairCnt,:) = SpikeAct;
-        outPair(numPairCnt,:) = [rowNew colNew];
-        
-        %Update the list of original pairs
-        tempPairs = outPairID{ICGlevel-1}([rowNew colNew],:);
-        outPairID{ICGlevel}(numPairCnt,:) = tempPairs(:);
-        
-        %Update list of available neurons to pair (optimised) 
-        gdIndex = allRowIndx ~= rowNew & allRowIndx ~= colNew & allColIndx ~= colNew & allColIndx ~= rowNew & gdIndex; 
-        
+
         %Find the next pairing (greedily)
         k = find(gdIndex,1,'first');
 
-    
-        
+        %Grabbing data
+        %top row index
+        rowNew = allRowIndx(k);
+        %top col index
+        colNew = allColIndx(k);
+
+        %ICG process
+        % Save data
+        outdat(numPairCnt,:) = ICGAct(rowNew,:) + ICGAct(colNew,:);
+
+        %Update the list of original pairs
+        outPairID{ICGlevel}(numPairCnt,:) = reshape(outPairID{ICGlevel-1}([rowNew colNew],:), 1, []);
+
+        %Update list of available neurons to pair (optimised)
+        gdIndex = allRowIndx ~= rowNew & allRowIndx ~= colNew & allColIndx ~= colNew & allColIndx ~= rowNew & gdIndex;
+
     end
-    fprintf('ICG level %2i completed in %f seconds\n\n', ICGlevel, toc); 
-    
-    
+    fprintf('ICG level %2i completed in %f seconds\n\n', ICGlevel, toc);
+
+
     %Save all the activity
     activityICG{ICGlevel} = outdat;
-    clearvars outdat outPair
-    
+    clearvars outdat
+
 end
 
 
